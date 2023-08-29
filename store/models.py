d0a402921914d8cf93edc7c768813dbe7c031718
Philip Moodie <philipmoodie1989@hotmail.co.uk>
2023-08-29 15:27:32 +0100

    added basket functionality

--- store/models.py
+++ store/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     product_id = models.IntegerField()
     product_name = models.CharField(max_length=128, unique=True)
@@ -8,17 +19,52 @@ class Product(models.Model):
     product_price = models.DecimalField(max_digits=6, decimal_places=2)
     product_stock = models.IntegerField(blank=True, null=True)
     product_image = models.ImageField(null=True, blank=True, upload_to="images/")
+    
     def __str__(self):
         return self.product_name
 
 
 class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
     order_id = models.CharField(max_length=128, unique=True)
-    order_qty = models.IntegerField(blank=True, null=True)
-    order_date = models.DateField(blank=True, null=True)
-    product_id = models.IntegerField(blank=True, null=True)
-    product_stock = models.IntegerField(blank=True, null=True)
-    user_id = models.CharField(max_length=128, unique=True)
+    order_date = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+
     def __str__(self):
         return self.order_id
     
+    @property
+    def get_basket_total(self):
+        order_items = self.orderitem_set.all()
+        total = sum([item.get_total for item in order_items])
+        return total
+    @property
+    def get_basket_items(self):
+        order_items = self.orderitem_set.all()
+        total = sum([item.quantity for item in order_items])
+        return total
+    
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.product_price * self.quantity
+        return total
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    country = models.CharField(max_length=200, null=True)
+    postcode = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.address
--- store/views.py
+++ store/views.py
@@ -9,7 +9,14 @@ def index(request):
     return render(request, 'store/index.html', context)
 
 def basket(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items - [] 
+        order = {'get_basket_total':0, 'get_basket_items':0}   
+    context = {'items' : items, 'order' : order}
     return render(request, 'store/basket.html', context)
 
 def checkout(request):
